764cb2dede6ee665229eac0f7fbf108a029718de
martyniukroman <hjbfrcgq@gmail.com>
2018-06-07 21:43:18 +0300

    fixed my sheet

--- adsLibrarySolution/adsLibrarySolution/MainWindow.xaml.cs
+++ adsLibrarySolution/adsLibrarySolution/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace adsLibrarySolution {
         public MainWindow() {
             InitializeComponent();
 
-            
+           this.ShowMessageAsync("Alert", "andriy sosat");
 
         }
     }

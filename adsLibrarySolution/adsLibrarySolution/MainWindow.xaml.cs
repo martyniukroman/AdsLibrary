76d062693dd7b7439e699bac1eb705d0c0cfb147
martyniukroman <hjbfrcgq@gmail.com>
2018-06-07 16:14:42 +0300

    Added Tile

--- adsLibrarySolution/adsLibrarySolution/MainWindow.xaml.cs
+++ adsLibrarySolution/adsLibrarySolution/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,9 +18,12 @@ namespace adsLibrarySolution {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow : MahApps.Metro.Controls.MetroWindow {
         public MainWindow() {
             InitializeComponent();
+
+            
+
         }
     }
 }

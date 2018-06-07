da5d798ca03c886f9344b1f3dba2607a8dd372eb
Усманаев Андрей <andrei.usmanaev@gmail.com>
2018-06-07 19:36:42 +0300

    added Client and Advert classes

---
+++ adsLibrarySolution/adsLibrarySolution/Advert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+namespace adsLibrarySolution
+{
+    class Advert
+    {
+        public int ID { get; set; }
+        public string Header { get; set; }
+        public string Information { get; set; }
+        public BitmapImage AdvertImage { get; set; }
+    }
+}
---
+++ adsLibrarySolution/adsLibrarySolution/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adsLibrarySolution
+{
+    class Client
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string FamilyName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
